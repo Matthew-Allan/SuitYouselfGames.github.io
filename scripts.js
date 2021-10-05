b1a65042f0983f762a9ff34264eb59f9382bf3cc
Matthew A <matthewgallan@gmail.com>
2021-10-05 09:32:36 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 window.addEventListener('resize', toggle);
 function toggle() {
-	if (document.getElementById("topnav").style.height = "130px"){
-		document.getElementById("topnav").style.height == "230px";
+	if (document.getElementById("topnav").style.height == "130px"){
+		document.getElementById("topnav").style.height = "230px";
 		console.log(document.getElementById("topnav").style.height);
 	}
 	else {
 		console.log("down");
-		document.getElementById("topnav").style.height == "130px";
+		document.getElementById("topnav").style.height = "130px";
 	}
 }
 function scroll_to_top() {
