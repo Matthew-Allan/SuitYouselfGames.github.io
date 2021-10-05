1074d7df07bca424d827c0861085bdc74ecb944a
Matthew A <matthewgallan@gmail.com>
2021-10-05 09:28:18 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ window.addEventListener('resize', toggle);
 function toggle() {
 	if (document.getElementById("topnav").style.height = "130px"){
 		document.getElementById("topnav").style.height = "230px";
-		console.log("up");
+		console.log(document.getElementById("topnav").style.height);
 	}
 	else {
 		console.log("down");
