d53c9b55dbe8a4372f8fb691cc701ec3644137a4
Matthew A <matthewgallan@gmail.com>
2021-10-18 13:32:03 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
 window.addEventListener('resize', move_menu_buttons);
-function toggle() {
-	if (document.getElementById("topnav").style.height == "230px"){
-		document.getElementById("topnav").style.height = "130px";
-		console.log(document.getElementById("topnav").style.height);
+window.addEventListener('load', set_topnav_height)
+window.addEventListener('load', move_menu_buttons)
+function set_topnav_height(){
+	if (window.innerHeight > window.innerWidth) {
+		topnavHeight = "310px";
 	}
 	else {
-		console.log("down");
-		document.getElementById("topnav").style.height = "230px";
+		topnavHeight = "230px";
+	}
+}
+function toggle() {
+	if (document.getElementById("topnav").style.height == topnavHeight){
+		document.getElementById("topnav").style.height = "130px";
+	}
+	else {
+		document.getElementById("topnav").style.height = topnavHeight;
 	}
 }
 function scroll_to_top() {
@@ -15,8 +23,24 @@ function scroll_to_top() {
 function move_menu_buttons() {
 	if (window.innerHeight > window.innerWidth) {
 		console.log("Mobile");
+		if (document.getElementById("topnav").style.height == topnavHeight){
+			document.getElementById("topnav").style.height = "310px";
+		}
+		topnavHeight = "310px";
+		document.getElementById("leftButton").id = "topButton"
+		document.getElementById("rightButton").id = "bottomButton"
+		document.getElementById("topButton").className = "menuLinkMobile"
+		document.getElementById("bottomButton").className = "menuLinkMobile"
 	}
 	else {
 		console.log("PC");
+		if (document.getElementById("topnav").style.height == topnavHeight){
+			document.getElementById("topnav").style.height = "230px";
+		}
+		topnavHeight = "230px";
+		document.getElementById("topButton").id = "leftButton"
+		document.getElementById("bottomButton").id = "rightButton"
+		document.getElementById("leftButton").className = "menuLink"
+		document.getElementById("rightButton").className = "menuLink"
 	}
 }
